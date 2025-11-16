27cedb8e1ec56f638c6de1ad65a8a49803d42d53
Shane Flandermeyer <shaneflandermeyer@gmail.com>
2025-11-16 17:24:35 -0600

    Clip value normalize scale to avoid amplifying small returns

--- tdmpc2_jax/tdmpc2.py
+++ tdmpc2_jax/tdmpc2.py
@@ -221,10 +221,11 @@ class TDMPC2(struct.PyTreeNode):
 
       if self.normalize_elite_values:
         # Normalize elites to make softmax invariant to value scale as in [1]
-        # [1] Williams2016 - Aggressive driving with model predictive integral control 
+        # [1] Williams2016 - Aggressive driving with model predictive integral control
         scale = self.normed_elite_scale / (
-            abs(elite_values).mean(axis=-1) + 1e-8
-        ) / self.temperature
+            self.temperature * abs(elite_values).mean(axis=-1).clip(1, None)
+            + 1e-8
+        )
         elite_values = scale[..., None] * elite_values
 
       # Update population distribution
